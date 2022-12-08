5d63cecfcb4a4461a6507aff7767cef92b9c892f
wuuu0 <748615542@qq.com>
2022-12-08 15:32:16 +0800

    feat: 5-4 JWT 原理和auth-provider实现

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -10,6 +10,7 @@ export interface User {
   email: string;
   title: string;
   organization: string;
+  token: string;
 }
 
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
