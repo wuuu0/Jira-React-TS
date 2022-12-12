4dc6d41faa502e47bcc8081f276b1c62df279027
wuuu0 <748615542@qq.com>
2022-12-12 14:27:19 +0800

    style: 安装 emotion，优化登陆注册界面样式

--- src/screens/unauthenticated-app/login.tsx
+++ src/screens/unauthenticated-app/login.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 // import { FormEvent } from "react";
 import { Form, Input, Button } from "antd";
+import { LongButton } from ".";
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
@@ -27,9 +28,9 @@ export const LoginScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type="primary">
+        <LongButton htmlType="submit" type="primary">
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/screens/unauthenticated-app/register.tsx
+++ src/screens/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 // import { FormEvent } from "react";
 import { Form, Input, Button } from "antd";
+import { LongButton } from ".";
 
 export const RegisterScreen = () => {
   const { register, user } = useAuth();
@@ -27,9 +28,9 @@ export const RegisterScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type="primary">
+        <LongButton htmlType="submit" type="primary">
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
